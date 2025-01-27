2f1489f3632dbe64dfd153b9b1e6b8b51ff67ec5
Katherine Powderly <katherine.powderly@pizzahut.com>
2025-01-26 23:01:38 -0600

    implemented solution with all tests passing

--- go/pkg/sieve/sieve.go
+++ go/pkg/sieve/sieve.go
@@ -1,9 +1,73 @@
 package sieve
 
+import (
+	"fmt"
+	"log"
+)
+
+const minimumPrime int64 = 2
+
 type Sieve interface {
-	NthPrime(n int64) int64
+	GetNthPrime(n int64) (int64, error)
 }
 
+// Prime struct implements the Sieve interface.
+type Prime struct{}
+
 func NewSieve() Sieve {
-	panic("unimplemented")
+	return &Prime{}
+}
+
+// GetNthPrime returns the nth prime number i.e. 2 as the 0th prime number.
+func (prime *Prime) GetNthPrime(n int64) (int64, error) {
+	scope := n
+	primeList := []int64{}
+
+	if n < 0 {
+		return 0, fmt.Errorf("invalid number: %d, negative numbers not allowed", n)
+	} else if n < minimumPrime {
+		scope = n + 1
+
+		log.Printf("%d is less than minimum prime: %d, using %d as scope", n, minimumPrime, scope)
+	}
+
+	// Scope is used to control the range of prime numbers to generate.
+	// If the list is not long enough, triple the scope to generate more prime numbers
+	// until the list is large enough to retrieve the nth prime number.
+	for int64(len(primeList)) <= n {
+		scope *= 3
+		primeList = prime.generateList(scope)
+	}
+
+	return primeList[n], nil
+}
+
+// generateList generates a list of prime numbers up to the limit.
+func (prime *Prime) generateList(limit int64) []int64 {
+	// Initialize slice of booleans to indicate numbers as prime.
+	isPrime := make([]bool, limit+1)
+	for i := range isPrime {
+		isPrime[i] = true
+	}
+
+	// Sieve of Eratosthenes
+	// Begin at the minimum prime number and mark all multiples of prime 
+	// numbers as not prime.
+	for i := minimumPrime; i <= limit; i++ {
+		if isPrime[i] {
+			for j := i * i; j <= limit; j += i {
+				isPrime[j] = false
+			}
+		}
+	}
+
+	// Assign prime numbers to list.
+	primeList := []int64{}
+	for i := minimumPrime; i <= limit; i++ {
+		if isPrime[i] {
+			primeList = append(primeList, i)
+		}
+	}
+
+	return primeList
 }
--- go/pkg/sieve/sieve_test.go
+++ go/pkg/sieve/sieve_test.go
@@ -1,31 +1,98 @@
-package sieve
+package sieve_test
 
 import (
+	"errors"
 	"math/big"
+	"ssse-exercise-sieve/pkg/sieve"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func TestNthPrime(t *testing.T) {
-	sieve := NewSieve()
-
-	assert.Equal(t, int64(2), sieve.NthPrime(0))
-	assert.Equal(t, int64(71), sieve.NthPrime(19))
-	assert.Equal(t, int64(541), sieve.NthPrime(99))
-	assert.Equal(t, int64(3581), sieve.NthPrime(500))
-	assert.Equal(t, int64(7793), sieve.NthPrime(986))
-	assert.Equal(t, int64(17393), sieve.NthPrime(2000))
-	assert.Equal(t, int64(15485867), sieve.NthPrime(1000000))
-	assert.Equal(t, int64(179424691), sieve.NthPrime(10000000))
-	//assert.Equal(t, int64(2038074751), sieve.NthPrime(100000000)) not required, just a fun challenge
+func TestGetNthPrime(t *testing.T) {
+	type args struct {
+		number int64
+	}
+	type want struct {
+		res int64
+		err error
+	}
+	tests := []struct {
+		name string
+		args *args
+		want *want
+	}{
+		{
+			name: "Error: Negative number is invalid",
+			args: &args{number: -1},
+			want: &want{res: 0, err: errors.New("invalid number: -1, negative numbers not allowed")},
+		},
+		{
+			name: "Success Case 1",
+			args: &args{number: 0},
+			want: &want{res: 2, err: nil},
+		},
+		{
+			name: "Success Case 2",
+			args: &args{number: 19},
+			want: &want{res: 71, err: nil},
+		},
+		{
+			name: "Success Case 3",
+			args: &args{number: 99},
+			want: &want{res: 541, err: nil},
+		},
+		{
+			name: "Success Case 4",
+			args: &args{number: 500},
+			want: &want{res: 3581, err: nil},
+		},
+		{
+			name: "Success Case 5",
+			args: &args{number: 986},
+			want: &want{res: 7793, err: nil},
+		},
+		{
+			name: "Success Case 6",
+			args: &args{number: 2000},
+			want: &want{res: 17393, err: nil},
+		},
+		{
+			name: "Success Case 7",
+			args: &args{number: 1000000},
+			want: &want{res: 15485867, err: nil},
+		},
+		{
+			name: "Success Case 8",
+			args: &args{number: 10000000},
+			want: &want{res: 179424691, err: nil},
+		},
+		{
+			name: "Success Case 9",
+			args: &args{number: 100000000},
+			want: &want{res: 2038074751, err: nil},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt // pin
+		t.Run(tt.name, func(t *testing.T) {
+			sieve := sieve.NewSieve()
+			got, err := sieve.GetNthPrime(tt.args.number)
+			require.Equal(t, tt.want.err, err)
+			require.Equal(t, tt.want.res, got)
+		})
+	}
 }
 
 func FuzzNthPrime(f *testing.F) {
-	sieve := NewSieve()
+	sieve := sieve.NewSieve()
 
 	f.Fuzz(func(t *testing.T, n int64) {
-		if !big.NewInt(sieve.NthPrime(n)).ProbablyPrime(0) {
+		sieve, err := sieve.GetNthPrime(n)
+		if err != nil {
+			t.Fatalf("unexpected error: %v", err)
+		}
+		if !big.NewInt(sieve).ProbablyPrime(0) {
 			t.Errorf("the sieve produced a non-prime number at index %d", n)
 		}
 	})
